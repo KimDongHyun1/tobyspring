5a47d225ea63fe05d56b1de78b79af05c93fde49
김동현 <donghyun4012@naver.com>
2023-07-15 09:48:09 +0900

    섹션3. 독립 실행형 서블릿 애플리케이션

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,17 @@
+package tobyspring.helloboot;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+}
